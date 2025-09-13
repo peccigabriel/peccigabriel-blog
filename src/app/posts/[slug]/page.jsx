5167c7d0acd6e49d493499f4904ddceafac98a0e
Gabriel Pecci <gabriel.pecci@rdstation.com>
2025-09-13 15:16:13 -0300

    feat: add image cover to first post

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -3,9 +3,10 @@ import path from "path";
 import matter from "gray-matter";
 import { formatDate } from "@/helpers/formatDate";
 import { MDXRemote } from "next-mdx-remote/rsc";
+import Image from "next/image";
 import { Provider } from "@/components/ui/provider";
 import { getMDXComponents } from "../../../../mdx-components";
-import { Text } from "@chakra-ui/react";
+import { Text, Heading, Box } from "@chakra-ui/react";
 
 export async function generateStaticParams() {
   const postsDir = path.join(process.cwd(), "content", "posts");
@@ -30,6 +31,17 @@ export default async function PostPage({ params }) {
         {formatDate(data.date)}
       </Text>
       <hr style={{ margin: "1rem 0" }} />
+      <Heading as="h1" size="3xl" mt="8" mb="4">
+        {data.title}
+      </Heading>
+      <Box mb={8} mt={8}>
+        <Image
+          src={data.cover}
+          alt={data.coverAlt}
+          width={1920}
+          height={1080}
+        />
+      </Box>
       <MDXRemote source={content} components={getMDXComponents()} />
     </Provider>
   );
